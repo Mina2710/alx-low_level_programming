e05aff0b55813ca0b2d3ccdd8d43a461e92eb434
Mina2710 <mena2017666@gmail.com>
2023-03-16 18:27:37 -0700

    init commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Printing ouput to the standard error
- *
- * Return: 1 (Success)
- * Using fprintf
- */
+ *  * main - Printing ouput to the standard error
+ *   *
+ *    * Return: 1 (Success)
+ *     * Using fprintf
+ **/
+
 int main(void)
 {
-fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+  fprintf(stderr,
+	  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+  return (1);
 }
