6a456a28e1ec5842b334bc0be2f870bb46cdd270
Mina2710 <mena2017666@gmail.com>
2023-03-21 16:38:14 -0700

    init  commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase
+ * kosomkm 
+ * Return: Nothing to return
+ */
+void print_alphabet(void)
+{
+int alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
