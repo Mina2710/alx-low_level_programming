d82da5fa8ad1827df1c9a80ab43503a1741f1f57
Mina2710 <mena2017666@gmail.com>
2023-03-21 16:15:39 -0700

    init commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,12 @@
-#include <stdio.h>
-
-void _putchar(char x) {
-printf("%c", x);
-printf("%s", (const char *) "\n");
+#include "main.h"
+int main(void)
+{
+char str[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(str[i]);
 }
-
-int main() {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return 0;
+_putchar('\n');
+return (0);
 }
